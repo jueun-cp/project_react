b374bc571990d4e8ae2f725b23077a51742a1c17
Yong <fkszm3@gmail.com>
2021-10-22 19:20:22 +0900

    Add JWT

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,18 +1,19 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 //// 리펙토링 + 미들웨어 추가 ////
 const post = new Router();
 post.get('/', postsCtrl.read); // ObjectId 검증이 필요한 부분에 미들웨어 추가
-post.delete('/', postsCtrl.remove);    // ObjectId 검증이 필요한 부분에 미들웨어 추가
-post.patch('/', postsCtrl.update); // ObjectId 검증이 필요한 부분에 미들웨어 추가
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove); // ObjectId 검증이 필요한 부분에 미들웨어 추가
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update); // ObjectId 검증이 필요한 부분에 미들웨어 추가
 
-posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+posts.use('/:id', postsCtrl.getPostById, post.routes());
 //// 리펙토링 + 미들웨어 추가 ////
 
 export default posts;
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,38 +1,44 @@
-import  Post from '../../models/post';
+import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
-
 
 ///// 코드 검증을 위한 미들웨어 작성 시작 /////
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
-  if(!ObjectId.isValid(id)) {
+  if (!ObjectId.isValid(id)) {
     ctx.status = 400; // Bad Request
     return;
   }
-  return next();
+  try {
+    const post = await Post.findById(id);
+    // 포스트가 존재하지 않을 때
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 ///// 코드 검증을 위한 미들웨어 작성 끝 /////
 
-
-
 ////// 블로그 포스트를 작성하는 API 시작//////
 // 데이터 쓰기(write)
-export const write = async ctx => {
+export const write = async (ctx) => {
   const schema = Joi.object().keys({
     // 객체가 다음 필드를 가지고 있음을 검증
     title: Joi.string().required(), // required()가 있으면 항목
     body: Joi.string().required(),
-    tags: Joi.array()
-      .items(Joi.string())
-      .required(),  // 문자열로 이루어진 배열
+    tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
   });
 
   // 검증하고 나서 검증 실패인 경우 에러 처리
   const result = schema.validate(ctx.request.body);
-  if(result.error){
+  if (result.error) {
     ctx.status = 400; // Bad Request
     ctx.body = result.error;
     return;
@@ -43,6 +49,7 @@ export const write = async ctx => {
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
   try {
     await post.save();
@@ -53,33 +60,39 @@ export const write = async ctx => {
 };
 
 // 데이터 조회(list)
-export const list = async ctx => {
+export const list = async (ctx) => {
   // query는 문자열이기 때문에 숫자로 변환해 주어야 한다.
   // 값이 주어지지 않았다면 1을 기본으로 사용한다.
 
   //// 페이지를 지정하여 조회가능 시작 ////
   const page = parseInt(ctx.query.page || '1', 10);
 
-  if(page < 1) {
+  if (page < 1) {
     ctx.status = 400;
     return;
   }
-  //// 페이지를 지정하여 조회가능 끝 ////
 
-  try{
+  const { tag, username } = ctx.query;
+  // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
+  try {
     const posts = await Post.find()
-    .sort({ _id: -1 })  // _id의 값이 1이면 오름차순, -1이면 내림차순으로 설정
-    .limit(10)   // 파라미터안에 제한할 숫자를 넣는다.
-    .skip((page - 1) * 10)  // 10페이지로 한번에 이동, 페이지를 지정하여 조회
-    .lean()   // lean()함수를 이용하여 글자 길이 제한
-    .exec();   // exec으로 서버에 쿼리를 요청
-    const postCount = await Post.countDocuments().exec();   // 마지막 페이지 알려주는 곳
-    ctx.set('Last-Page', Math.ceil(postCount / 10));    // 마지막 페이지 알려주는 곳
-    ctx.body = posts.map(post => ({
-        ...post,
-        body:
-          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,   // 글자길이를 200자로 제한
-      }));
+      .sort({ _id: -1 }) // _id의 값이 1이면 오름차순, -1이면 내림차순으로 설정
+      .limit(10) // 파라미터안에 제한할 숫자를 넣는다.
+      .skip((page - 1) * 10) // 10페이지로 한번에 이동, 페이지를 지정하여 조회
+      .lean() // lean()함수를 이용하여 글자 길이 제한
+      .exec(); // exec으로 서버에 쿼리를 요청
+    const postCount = await Post.countDocuments().exec(); // 마지막 페이지 알려주는 곳
+    ctx.set('Last-Page', Math.ceil(postCount / 10)); // 마지막 페이지 알려주는 곳
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`, // 글자길이를 200자로 제한
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -89,36 +102,35 @@ export const list = async ctx => {
 /*
   POST /api/posts/:id
 */
-export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if(!post) {
-      ctx.status = 404; // Not Found
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+export const read = async (ctx) => {
+  ctx.body = ctx.state.post;
 };
 
 // 데이터 삭제(remove)
 /*
   POST /api/posts/:id
 */
-export const remove = async ctx => {
+export const remove = async (ctx) => {
   const { id } = ctx.params;
   try {
-    await Post.findByIdAndRemove(id).exec();  // id삭제 함수 사용, 다른걸 지우려면 remove() or findOneAndRemove() 사용
-    ctx.status = 204;   // No Connect (성공하기는 했지만 응답할 데이터가 없음)
+    await Post.findByIdAndRemove(id).exec(); // id삭제 함수 사용, 다른걸 지우려면 remove() or findOneAndRemove() 사용
+    ctx.status = 204; // No Connect (성공하기는 했지만 응답할 데이터가 없음)
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
+};
 
 // 데이터 수정(update)
+
 /*
   PATCH /api/posts/:id
   {
@@ -127,7 +139,7 @@ export const remove = async ctx => {
     tags: ['수정', '태그']
   }
 */
-export const update = async ctx => {
+export const update = async (ctx) => {
   const { id } = ctx.params;
   // write에서 사용한 schema와 비슷하지만, required()가 없다.
   const schema = Joi.object().keys({
@@ -138,21 +150,21 @@ export const update = async ctx => {
 
   // 검증하고 나서 검증 실패인 경우 에러 처리
   const result = schema.validate(ctx.request.body);
-  if(result.error){
+  if (result.error) {
     ctx.status = 400; // Bad Request
     ctx.body = result.error;
     return;
   }
 
-  try{
+  try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-      new: true,  // 이 값을 설정하면 업데이트된 데이터를 반환한다.
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환한다.
       // false일 때는 업데이트되기 전의 데이터를 반환한다.
     }).exec();
-    if(!post) {
+    if (!post) {
       ctx.status = 404;
       return;
-    } 
+    }
     ctx.body = post;
   } catch (e) {
     ctx.throw(500, e);
